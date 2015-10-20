9290afacb12d09338de0fbf59007b6a9257b68dc
jbhatab <jbhatab@gmail.com>
2015-10-20 13:15:02 -0600

    got channel working for rooms

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,11 +2,11 @@ defmodule PulseApi.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "rooms:*", PulseApi.RoomChannel
+  channel "rooms:*", PulseApi.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
+  transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
