490ed531c57e2730e135dfa6e40657bad00f011a
jbhatab <jbhatab@gmail.com>
2015-10-26 20:50:59 -0500

    added change username

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -6,23 +6,42 @@ defmodule PulseApi.RoomChannel do
 
   def join("rooms:lobby", message, socket) do
     Process.flag(:trap_exit, true)
-    :timer.send_interval(5000, :ping)
     send(self, {:after_join, message})
 
     {:ok, socket}
   end
+
+  # def join("rooms:" <> room_name, params, socket) do    
+  #   unless Repo.get_by(Room, name: room_name) do
+  #     Repo.insert! %Room{name: room_name}
+  #   end
+
+  #   send(self, {:after_join, params})
+  #   {:ok, RoomUsers.get_room(room_name), socket}
+  # end
+
+  # def join("rooms:" <> room_name, payload, socket) do
+  #   payload = atomize_keys(payload)
+  #   if authorized?(payload) do
+  #     unless Repo.get_by(Room, name: room_name) do
+  #       Repo.insert! %Room{name: room_name}
+  #     end
+  #     RoomActivityService.register(room: room_name, user: payload.user.name, pid: socket.channel_pid)
+  #     send(self, %{event: "user:joined", new_user: payload.user.name})
+  #     {:ok, RoomUsers.get_room(room_name), socket}
+  #   else
+  #     {:error, %{reason: "unauthorized"}}
+  #   end
+  # end
 
   def join("rooms:" <> _private_subtopic, _message, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
 
   def handle_info({:after_join, msg}, socket) do
-    broadcast! socket, "user:entered", %{user: msg["user"]}
-    push socket, "join", %{status: "connected"}
-    {:noreply, socket}
-  end
-  def handle_info(:ping, socket) do
-    push socket, "new:msg", %{user: "SYSTEM", body: "ping"}
+    push socket, "user:entered", %{user: msg["username"]}
+
+    # push socket, "join", %{status: "connected"}
     {:noreply, socket}
   end
 
@@ -37,9 +56,15 @@ defmodule PulseApi.RoomChannel do
 
     case PulseApi.Repo.insert(changeset) do
       {:ok, room} ->
-        broadcast! socket, "new:msg", %{user: msg["user"], body: msg["body"], room_id: msg["room_id"]}
+        push socket, "new:msg", %{user: msg["user"], body: msg["body"], room_id: msg["room_id"]}
         {:reply, {:ok, %{msg: msg["body"]}}, assign(socket, :user, msg["user"])}
       {:error, changeset} ->
     end
+  end
+
+  def handle_in("change:username", user, socket) do
+    push socket, "user:set_username", %{username: user["username"]}
+
+    {:noreply, socket}
   end
 end
